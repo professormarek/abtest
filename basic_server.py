49e96b5d57291d4a803519f5d74ee7fdd5322195
marek <marek@test.com>
2017-03-08 12:43:50 -0500

    handles post requests

--- basic_server.py
+++ basic_server.py
@@ -20,6 +20,17 @@ HTML = """
         </form>
     </body>
 </html>"""
+
+THANKS = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Test Site</title>
+    </head>
+    <body>
+        Thanks for your bitcoins mwa ha ha
+    </body>
+</html>"""
 #in order to respond to http requests we need to create a custom handler class
 class myHandler(BaseHTTPRequestHandler):
 	#declare a method to handle GET requests so that we can serve a page!
@@ -33,6 +44,15 @@ class myHandler(BaseHTTPRequestHandler):
 		#also send some html for the browser to render
 		#wfile is an object where we can write the response to the broswer
 		self.wfile.write(HTML)
+
+	#declare a method to handle http POST requests
+	def do_POST(self):
+		print "LOG: We received a POST request!"
+		self.send_response(200)
+		#send http header
+		self.send_header('Content-type', 'text/html')
+		self.end_headers()
+		self.wfile.write(THANKS)
 
 try:
 	#create a web server and specify the handler class for requests
